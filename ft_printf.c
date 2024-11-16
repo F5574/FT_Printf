6956cc61a81e3cb1f1ebfe4d45e333ef1111186a
F5574 <gabrielvonah04@outlook.com>
2024-11-16 20:01:01 +0000

    ! test

--- ft_printf.c
+++ ft_printf.c
@@ -6,64 +6,88 @@
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:34:59 by gvon-ah-          #+#    #+#             */
-/*   Updated: 2024/11/16 17:51:04 by gvon-ah-         ###   ########.fr       */
+/*   Updated: 2024/11/16 19:56:20 by gvon-ah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-void	aux(int c)
+int	aux_str(char *src)
 {
+	if (!src)
+		return (write(1, "(null)", 6));
+	else
+		return (write(1, src, ft_strlen(src)));
+}
+
+int	aux_ptr(unsigned long int ptr)
+{
+	int	i;
+
+	i = 0;
+	if (!ptr)
+	{
+		i += write (1, "(nil)", 5);
+	}
+	else
+	{
+		i += write(1, "0x", 2);
+		i += aux_putpt(ptr, EX_LOW_BASE, 0);
+	}
+	return (i);
+}
+
+static int	is_char(const char *src, const char c)
+{
+	while (*src)
+	{
+		if (*src == c)
+			return (1);
+		*src++;
+	}
+	return (0);
+}
+
+int	aux_flag(int c, va_list *args)
+{
+	int	i;
+
+	i = 0;
 	if (c == 'c')
-	{
-		
-	}
-	else if (c == 's')
-	{
-
-	}
-	else if (c == 'p')
-	{
-		
-	}
-	else if (c == 'd')
-	{
-
-	}
-	else if (c == 'i')
-	{
-		
-	}
-	else if (c == 'u')
-	{
-
-	}
-	else if (c == 'x')
-	{
-		
-	}
-	else if (c == 'X')
-	{
-
-	}
-	else if (c == '%')
-	{
-		
-	}
+		i += write(1, va_arg(*args, int), 1);
+	if (c == 's')
+		i += aux_str(va_arg(*args, char *));
+	if (c == 'p')
+		i += aux_ptr(va_arg(*args, long int));
+	if (c == 'd' || c == 'i')
+		i += aux_putnbr(va_arg(*args, int), 0);
+	if (c == 'u')
+		i += aux_putunbr(va_arg(*args, unsigned int), 0);
+	if (c == 'x')
+		i += aux_putbs(va_arg(*args, long int), EX_LOW_BASE, 0);
+	if (c == 'X')
+		i += aux_putbs(va_arg(*args, long int), EX_UP_BASE, 0);
+	if (c == '%')
+		i += write(1, "%", 1);
+	return (i);
 }
 
 int	ft_printf(const	char *src, ...)
 {
-	int	len;
+	int		len;
+	va_list	args;
 
 	if (!src)
 		return (-1);
+	len = 0;
+	va_start(args, src);
 	while (*src)
 	{
-		if (*src == '%')
-			aux(src + 1);
+		if (*src == '%' && is_char(POSSIBLE_FLAGS, src +1))
+			len += aux_flag(src + 1, &args);
 		else if (*src)
-			write(1, src, 1);
+			len += write(1, src, 1);
 		*src++;
 	}
+	return (len);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: gvon-ah- <gvon-ah-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 16:17:27 by gvon-ah-          #+#    #+#             */
-/*   Updated: 2024/11/16 16:34:53 by gvon-ah-         ###   ########.fr       */
+/*   Updated: 2024/11/16 19:59:00 by gvon-ah-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,16 @@
 # define FT_PRINTF_H
 
 # define EX_LOW_BASE "0123456789abcdef"
-# define EX_LOW_BASE "0123456789ABCDEF"
+# define EX_UP_BASE "0123456789ABCDEF"
 # define POSSIBLE_FLAGS "cspdiuxX%"
 
 # include "libft/libft.h"
 # include <stdarg.h>
 
 int	ft_printf(const	char *src, ...);
+int	aux_putbs(unsigned long int num, char *bs, int tt);
+int	aux_putpt(unsigned long int num, char *bs, int tt);
+int	aux_putnbr(int num, int tt);
+int	aux_putunbr(unsigned int num, int tt);
 
 #endif
